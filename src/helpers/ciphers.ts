37a745612255c3a3beb98c317e95a028fec6c97b
Dipadana Putu <dipadanadev@gmail.com>
2024-03-16 12:01:38 +0800

    chore: update chipher logic algorithm

--- src/helpers/ciphers.ts
+++ src/helpers/ciphers.ts
@@ -1,23 +1,28 @@
 // Resource for this code
 // https://fireship.io/lessons/node-crypto-examples
 // https://sl.bing.net/fVIcrPcSk1I
+// https://stackoverflow.com/questions/74177757/iam-getting-bad-decrypt-error-in-node-js
 
 import { createCipheriv, createDecipheriv, randomBytes, createHash } from 'crypto'
-
-const iv = randomBytes(16)
 
 function simpleHash(str: string): string {
   return createHash('sha256').update(String(str)).digest('hex').substring(0, 32)
 }
 
 function encrypt(payload: string, secret: string) {
+  const iv = randomBytes(16)
   const cipher = createCipheriv('aes256', simpleHash(secret), iv)
-  return cipher.update(payload, 'utf8', 'hex') + cipher.final('hex')
+
+  // Store the iv into the end of chipher string, divide by comma, and convert it to base64
+  return `${cipher.update(payload, 'utf8', 'hex')}${cipher.final('hex')},${iv.toString('base64')}`
 }
 
 function decrypt(encryptedMessage: string, secret: string) {
+  // Get the iv by splitting the string by comma and converting it to buffer
+  const splitEncryptedMessage = encryptedMessage.split(',')
+  const iv = Buffer.from(splitEncryptedMessage[1], 'base64')
   const decipher = createDecipheriv('aes256', simpleHash(secret), iv)
-  return decipher.update(encryptedMessage, 'hex', 'utf-8') + decipher.final('utf8')
+  return `${decipher.update(splitEncryptedMessage[0], 'hex', 'utf8')}${decipher.final('utf8')}`
 }
 
 export { encrypt, decrypt }
