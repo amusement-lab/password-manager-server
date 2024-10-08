65692513f21a1e55f667df8769466f145f079a65
dipadana <dipadanadev@gmail.com>
2024-10-08 12:17:31 +0800

    chore: update depedencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,7 @@
-import tsconfigPaths from 'vite-tsconfig-paths'
 import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude, 'tmp'],
   },
-  plugins: [tsconfigPaths()],
 })
